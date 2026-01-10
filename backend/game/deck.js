8494af4d2f3a881f92da24115d3bd2e7b44ede6c
Teke22 <ddm_dimitrov@mail.ru>
2026-01-10 18:52:31 +0300

    fix: add draw() method to deck

--- backend/game/deck.js
+++ backend/game/deck.js
@@ -1,22 +1,22 @@
 class Deck {
   constructor() {
-    this.suits = ['♠', '♥', '♦', '♣'];
-    this.ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
-    this.cards = [];
-    this.createDeck();
-    this.shuffle();
+    this.reset();
   }
 
-  createDeck() {
+  reset() {
     this.cards = [];
-    for (const suit of this.suits) {
-      for (const rank of this.ranks) {
+    const suits = ['♠', '♥', '♦', '♣'];
+    const ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+
+    for (const suit of suits) {
+      for (const rank of ranks) {
         this.cards.push({ rank, suit });
       }
     }
+
+    this.shuffle();
   }
 
-  // Алгоритм тасования Фишера-Йетса (используется в Python-боте)
   shuffle() {
     for (let i = this.cards.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
@@ -24,31 +24,11 @@ class Deck {
     }
   }
 
-  deal() {
+  draw() {
     if (this.cards.length === 0) {
       throw new Error('Deck is empty');
     }
     return this.cards.pop();
-  }
-
-  dealMultiple(count) {
-    const dealtCards = [];
-    for (let i = 0; i < count; i++) {
-      if (this.cards.length === 0) break;
-      dealtCards.push(this.deal());
-    }
-    return dealtCards;
-  }
-
-  // Оставшиеся карты в колоде
-  remaining() {
-    return this.cards.length;
-  }
-
-  // Перетасовать заново
-  reshuffle() {
-    this.createDeck();
-    this.shuffle();
   }
 }
 
