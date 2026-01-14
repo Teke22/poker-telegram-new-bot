2db0561f8f148fa6d95c8361edc3c2a5c0731644
Teke22 <ond38259@gmail.com>
2026-01-14 12:25:23 +0300

    14015

--- backend/game/config.js
+++ backend/game/config.js
@@ -2,5 +2,5 @@ module.exports = {
   SMALL_BLIND: 10,
   BIG_BLIND: 20,
   START_CHIPS: 1000,
-  NEXT_HAND_DELAY: 10000 // 3 секунды для авто-перезапуска
+  NEXT_HAND_DELAY: 3000 // 3 секунды для авто-перезапуска
 };
