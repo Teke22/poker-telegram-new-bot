b6be6906221529b29d3ba8f13c64a33a2cfc9400
Teke22 <ddm_dimitrov@mail.ru>
2026-01-17 15:55:35 +0300

    Refactor: split backend and frontend architecture5

--- backend/game/Game.js
+++ backend/game/Game.js
@@ -1,46 +1,24 @@
-const Deck = require('./engine/Deck');
-const BettingEngine = require('./engine/BettingEngine');
-const HandEvaluator = require('./engine/HandEvaluator');
-const TurnManager = require('./engine/TurnManager');
+const GameState = require('./gameState');
 
 class Game {
   constructor(players) {
-    this.players = players;
-    this.deck = new Deck();
-    this.betting = new BettingEngine(this);
-    this.turns = new TurnManager(this);
-    this.evaluator = new HandEvaluator();
-
-    this.stage = 'preflop';
-    this.communityCards = [];
-    this.pot = 0;
-    this.finished = false;
+    this.gameState = new GameState(players);
   }
 
   start() {
-    this.deck.shuffle();
-    this.players.forEach(p => p.deal(this.deck.draw(2)));
-    this.turns.start();
+    this.gameState.startGame();
   }
 
   applyAction(playerId, action) {
-    this.betting.apply(playerId, action);
-    this.turns.next();
-  }
-
-  finish() {
-    this.finished = true;
-    return this.evaluator.evaluate(this.players, this.communityCards);
+    this.gameState.handleAction(playerId, action);
   }
 
   getPublicState() {
-    return {
-      stage: this.stage,
-      pot: this.pot,
-      players: this.players.map(p => p.public()),
-      communityCards: this.communityCards,
-      currentPlayerId: this.turns.currentPlayerId()
-    };
+    return this.gameState.getPublicState();
+  }
+
+  get players() {
+    return this.gameState.players;
   }
 }
 
